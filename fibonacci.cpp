32098ad7c039805d9315f68ddffabdef7579bb77
Francisco Arrieta <francisco.arrieta@aggiemail.usu.edu>
2016-08-25 09:16:02 -0600

    Fibonacci sequence using for loops and recursion

---
+++ fibonacci.cpp
@@ -0,0 +1,52 @@
+//
+//  fibonacci.cpp
+//  Algorithms
+//
+//  Created by Francisco Arrieta on 8/25/16.
+//  Copyright © 2016 lil9porkchop. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int fibonacci_recursive(int);
+int fibonacci_loop(int);
+
+int main() {
+	
+	int x = 5;
+	cout << fibonacci_recursive(x) << endl;
+	cout << fibonacci_loop(x) << endl;
+	return 0;
+}
+
+
+int fibonacci_recursive(int nth) {
+	
+	if(nth < 2) {
+		return 1;
+	} else {
+		return fibonacci_recursive(nth - 2) + fibonacci_recursive(nth - 1);
+	}
+	
+	return 0;
+}
+
+int fibonacci_loop(int nth) {
+	
+	if(nth < 2) {
+		return 1;
+	}
+	
+	int firstPreceding = 1;
+	int secondPreceding = 1;
+	
+	for (int i = 1; i < nth; i++) {
+		
+		firstPreceding = secondPreceding + firstPreceding;
+		secondPreceding = firstPreceding - secondPreceding;
+		
+	}
+	
+	return firstPreceding;
+}
