5b0b4eb38849caeeb2feb18cfbfc568faeb955ca
Francisco Arrieta <francisco.arrieta@aggiemail.usu.edu>
2016-12-05 01:10:33 -0700

    Uploading the correct file for bucket sort

--- Python/bucketSort.py
+++ Python/bucketSort.py
@@ -1,15 +1,37 @@
-def bucketSort(A):
-	bucket = [[]]  # create an empty bucket
+def bucket_sort(A):
+    """
+    A bucket is kinda like a 2D array where each row could have a different number of columns
+    :param A: type(list)
+    :return:
+    """
 
-	# put array elements in different buckets
-	for i in range(0, len(A)):
-		bucket_index = len(A) * A[i]
-		bucket[bucket_index] = bucket_index
+    if not isinstance(A, list):
+        return
 
+    bucket = []  # create an empty bucket
 
-	return bucket
+    # initialize the bucket
+    for i in range(0, len(A)):
+        bucket.append([])
 
-arr = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434]
-arr = bucketSort(arr)
+    for i in range(0, len(A)):
+        index = int(len(A) * A[i])
+        bucket[index].append(A[i])
 
-print arr	
+    # Sort individual buckets. This could be replaced with a custom sort function
+    for i in range(0, len(A)):
+        bucket[i].sort()
+
+    # Transfer bucket back into the array
+    index = 0
+    for i in range(0, len(A)):
+        for j in range(0, len(bucket[i])):
+            A[index] = bucket[i][j]
+            index += 1
+
+    print bucket
+    print A #  <- Final solution
+
+arr = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434, 0.1113]
+bucket_sort(arr)
+
